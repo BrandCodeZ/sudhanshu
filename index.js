82c0dc420268d4ec98828b43cdb625dacf0a9c80
sudhanshu Sahu <sudhanshukumar40234@gmail.com>
2026-07-26 22:42:11 +0530

    update and change all the necesssary file

--- index.js
+++ index.js
@@ -1,18 +1,40 @@
-const simpleGit = require('simple-git');
-const jsonfile = require('jsonfile');
-const moment = require('moment');
+import jsonfile from "jsonfile";
+import moment from "moment";
+import simpleGit from "simple-git";
+import random from "random";
 
-const path = './data.json';
-const date = moment().format('YYYY-MM-DD HH:mm:ss');
+const path = "./data.json";
 
-const data = {
-  date: date,
+const markCommit = (x, y) => {
+  const date = moment()
+    .subtract(1, "y")
+    .add(1, "d")
+    .add(x, "w")
+    .add(y, "d")
+    .format();
+
+  const data = {
+    date: date,
+  };
+
+  jsonfile.writeFile(path, data, () => {
+    simpleGit().add([path]).commit(date, { "--date": date }).push();
+  });
 };
 
-jsonfile.writeFileSync(path, data);
+const makeCommits = (n) => {
+  if(n===0) return simpleGit().push();
+  const x = random.int(0, 54);
+  const y = random.int(0, 6);
+  const date = moment().subtract(1, "y").add(1, "d").add(x, "w").add(y, "d").format();
 
-const git = simpleGit();
-git.add([path])
-  .then(() => git.commit(date))
-  .then(() => git.push())
-  .catch((err) => console.error('Git operation failed:', err.message));
+  const data = {
+    date: date,
+  };
+  console.log(date);
+  jsonfile.writeFile(path, data, () => {
+    simpleGit().add([path]).commit(date, { "--date": date },makeCommits.bind(this,--n));
+  });
+};
+
+makeCommits(100);
